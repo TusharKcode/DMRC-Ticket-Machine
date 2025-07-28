4d207f233ebbdc0c54b16b09d015954c9b777c19
Tushar Kumar <tkgoyal104@gmail.com>
2025-07-28 11:51:59 +0530

    Initial Commit-Recharge Card Screen File Updated

--- src/main/java/org/example/RechargeCardScreen.java
+++ src/main/java/org/example/RechargeCardScreen.java
@@ -4,7 +4,9 @@ import javafx.animation.PauseTransition;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -31,6 +33,41 @@ public class RechargeCardScreen {
     }
 
     public static void createCardDetailsScreen(Stage primaryStage){
+        //------------------------------------------------>>>> Dummy Card
+        String cardNumber = "DMRC123456789";
+        double currentBalance = 180.0;
+        double maxBalance = 3000.0;
 
+        Label title = new Label("Card Details");
+        title.setStyle("-fx-font-size: 24px, -fx-font-weight: bold");
+
+        Label cardlabel = new Label("Card Number: " + cardNumber);
+        Label balanceLabel =new Label("Current Balance: " + currentBalance);
+        Label maxLabel = new Label("Maximum Allowed Balance: " + maxBalance);
+
+        VBox cardInfoBox = new VBox();
+        cardInfoBox.setStyle("-fx-background-color: #ffffff, -fx-background-radius: 8px, -fx-border-color: #ccc, -fx-border-radius: 8px,");
+        cardInfoBox.setPadding(new Insets(20));
+        cardInfoBox.setAlignment(Pos.CENTER_LEFT);
+
+        Button cashBtn = new Button("Cash Payment");
+        Button onlineBtn = new Button("Online Payment");
+
+        cashBtn.setStyle("-fx-font-size: 16px");
+        cashBtn.setOnAction(e -> createCashPaymentScreen(primaryStage, cardNumber, currentBalance, maxBalance));
+
+        onlineBtn.setStyle("-fx-font-size: 16px");
+        onlineBtn.setOnAction(e -> createOnlinePaymentScreen(primaryStage, cardNumber, currentBalance, maxBalance));
+
+        HBox buttonBox = new HBox(30, cashBtn, onlineBtn);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        VBox root = new VBox(30, cardInfoBox, buttonBox);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(40));
+        root.setStyle("-fx-background-color: #f2f2f2");
+
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setScene(scene);
     }
 }
